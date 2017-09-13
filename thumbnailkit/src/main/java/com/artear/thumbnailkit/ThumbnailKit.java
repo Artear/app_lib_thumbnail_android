f97ab57b939c9a467cf31b025743b09dd53baa35
SergioB <sergio.banares@gmail.com>
2017-09-13 14:39:27 -0300

    Change: initial for testing

---
+++ thumbnailkit/src/main/java/com/artear/thumbnailkit/ThumbnailKit.java
@@ -0,0 +1,13 @@
+package com.artear.thumbnailkit;
+
+import android.util.Log;
+
+/**
+ * Created by sergiobanares on 13/9/17.
+ */
+
+public class ThumbnailKit {
+    public static void Track(){
+        Log.d("MultiTrack", "First commit");
+    }
+}
